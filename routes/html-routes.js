0c2a3f14f1f057ca9f0f7dbdad4ff6c16d9523bf
jonabari <barcelo.jonathan@gmail.com>
2018-12-02 13:15:16 -0600

    cors fix #7.1

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,19 +5,16 @@ module.exports = function (app) {
     app.get('/', function (req, res, next) {
         res.sendFile(path.join(__dirname, ('../public/index.html')))
     })
-    app.get('/:cities', function (req, res, next) {
-        res.json({ msg: 'listening for cities!' })
-    })
-    app.get('/cdmx', function (req, res) {
+    app.get('/cdmx', function (req, res, next) {
         res.sendFile(path.join(__dirname, (`../public/cities/cdmx.html`)))
     })
-    app.get('/nyc', function (req, res) {
+    app.get('/nyc', function (req, res, next) {
         res.sendFile(path.join(__dirname, (`../public/cities/nyc.html`)))
     })
-    app.get('/dallas', function (req, res) {
+    app.get('/dallas', function (req, res, next) {
         res.sendFile(path.join(__dirname, (`../public/cities/dallas.html`)))
     })
-    app.get('/denver', function (req, res) {
+    app.get('/denver', function (req, res, next) {
         res.sendFile(path.join(__dirname, (`../public/cities/denver.html`)))
     })
 }
