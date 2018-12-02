178809e4008d813aec59d01b388bf9b019047e99
jonabari <barcelo.jonathan@gmail.com>
2018-12-02 13:13:08 -0600

    cors fix attempt #7

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,33 +3,21 @@ const path = require('path')
 module.exports = function (app) {
 
     app.get('/', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.sendFile(path.join(__dirname, ('../public/index.html')))
-        next();
     })
-    app.get('/cdmx', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    app.get('/:cities', function (req, res, next) {
+        res.json({ msg: 'listening for cities!' })
+    })
+    app.get('/cdmx', function (req, res) {
         res.sendFile(path.join(__dirname, (`../public/cities/cdmx.html`)))
-        next();
     })
-    app.get('/nyc', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    app.get('/nyc', function (req, res) {
         res.sendFile(path.join(__dirname, (`../public/cities/nyc.html`)))
-        next();
     })
-    app.get('/dallas', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    app.get('/dallas', function (req, res) {
         res.sendFile(path.join(__dirname, (`../public/cities/dallas.html`)))
-        next();
     })
-    app.get('/denver', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    app.get('/denver', function (req, res) {
         res.sendFile(path.join(__dirname, (`../public/cities/denver.html`)))
-        next();
     })
 }
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 8080
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+app.use(cors())
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'));
