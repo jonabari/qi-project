1465d0e53f0975ef93afab2ada9473dd5150b883
jonabari <barcelo.jonathan@gmail.com>
2018-12-02 12:37:14 -0600

    CORS fix attempt #2

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const path = require('path')
 
 const PORT = process.env.PORT || 8080
 
+let allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(serveStatic(path.join(__dirname, 'public')))
