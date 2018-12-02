688bd5310ef4717f94ee541c32d83794b9c520cf
jonabari <barcelo.jonathan@gmail.com>
2018-12-02 13:24:04 -0600

    cors fix attempt #8

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(cors())
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'));
