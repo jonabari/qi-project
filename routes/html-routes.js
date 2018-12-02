f3988f82fac89f026edc1eccccfa3f36d722edbc
jonabari <barcelo.jonathan@gmail.com>
2018-12-02 13:00:19 -0600

    cors fix 6.1

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,31 +5,31 @@ module.exports = function (app) {
     app.get('/', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
         res.sendFile(path.join(__dirname, ('../public/index.html')))
+        next();
     })
     app.get('/cdmx', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
         res.sendFile(path.join(__dirname, (`../public/cities/cdmx.html`)))
+        next();
     })
     app.get('/nyc', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
         res.sendFile(path.join(__dirname, (`../public/cities/nyc.html`)))
+        next();
     })
     app.get('/dallas', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
         res.sendFile(path.join(__dirname, (`../public/cities/dallas.html`)))
+        next();
     })
     app.get('/denver', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
         res.sendFile(path.join(__dirname, (`../public/cities/denver.html`)))
+        next();
     })
 }
